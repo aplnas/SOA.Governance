44d2b5c0efeeeb3dbd5843540b888169c48f37f2
collectivelymad <mikey.drex@gmail.com>
2016-02-05 11:12:07 -0800

    add to repo base classes

--- Infrastructure.Contracts/publishLocal.bat
+++
@@ -1,5 +0,0 @@
-﻿msbuild Infrastructure.Contracts.csproj /target:Rebuild /property:Configuration=Debug
-nuget pack Infrastructure.Contracts.csproj -Symbols -Prop Configuration=Debug
-move *.nupkg C:\LocalNuget
-
- 
